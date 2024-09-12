467e7c3031a5be45ecc6d3177e3afa33c4b4e738
Yousef M <yousefmalsaadan@gmail.com>
2024-09-12 11:03:50 +0300

    Half the solution

---
+++ main.js
@@ -0,0 +1,9 @@
+let text = document.getElementById("text");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+
+text.classList.add('change_text');
+btn1.classList.add('change_text');
+btn2.classList.add('change_text');
+btn3.classList.add('change_text');
